779e2492a8c318061569954dec911712e6d1c273
Neeraj Kulkarni <nvkulkarni.1108@gmail.com>
2026-06-12 15:19:40 +0530

    Add Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,24 @@ export default function RootLayout({
       className={`${grotesk.variable} ${plex.variable} ${newsreader.variable} h-full antialiased`}
       suppressHydrationWarning
     >
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
       <head>
         {/* applies the saved console theme before first paint */}
         <Script
